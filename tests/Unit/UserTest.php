985d21c7e1a4d81c9ced91e6f8955b7c6665b0c2
aniket mirajkar <aam257@njit.edu>
2018-12-05 16:33:38 -0500

    Task: User test modified for answers object

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -30,4 +30,10 @@ class UserTest extends TestCase
         $user = factory(\App\User::class)->make();
         $this->assertTrue(is_object($user->profile()->get()));
     }
+
+    public function testAnswers()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->answers()->get()));
+    }
 }
