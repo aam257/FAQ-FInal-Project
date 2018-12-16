ddb2553f7cdb811bbbf3594870fa0b1659c8c75e
aniket mirajkar <aam257@njit.edu>
2018-12-16 15:02:20 -0500

    Task: Laravel Dusk tests created for Laravel Scout search functionality.

---
+++ tests/Browser/SearchTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Chrome;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+use App\Answer;
+use App\Question;
+use App\User;
+
+class SearchTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+
+    public function testLoginNSearch()
+    {
+        $user = factory(User::class)->make();
+        $user->save();
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertSee('Search');
+        });
+    }
+
+    public function  testSearch()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/home')
+                ->type('search','hi')
+                ->press('Search')
+                ->assertSee('hi');
+        });
+    }
+
+
+}
